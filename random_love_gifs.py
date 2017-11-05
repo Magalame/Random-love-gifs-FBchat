e9524e796c42bbe9eb18f0c47777e62e390aac76
LawLawL <nbaspin@gmail.com>
2017-11-04 21:00:34 -0300

    Added arguments

--- random_love_gifs.py
+++ random_love_gifs.py
@@ -7,38 +7,98 @@ import random
 import time
 import sys
 import argparse
+import getpass
+import os.path
+
 
 #------------------------Parsing arguments
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-s','--start_time', action="store", dest="start_time")
-parser.add_argument('-P','--stop_time', action="store", dest="stop_time")
-parser.add_argument('-p','--password', action="store", dest="password")
-parser.add_argument('-a','--email_address', action="store", dest="address")
-parser.add_argument('-d','--destination_user', action="store", dest="destination")
-parser.add_argument('-i','--user_id', action="store", dest="user_id")
+parser.add_argument('-s','--start_time', action="store", dest="start_time", help="Starting time of the gif-sending period")
+parser.add_argument('-p','--stop_time', action="store", dest="stop_time", help="Stoping time of the gif-sending period")
+parser.add_argument('-P','--password', action="store", dest="password", help="Your facebook password")
+parser.add_argument('-e','--email_address', action="store", dest="address", help="Your email address")
+parser.add_argument('-d','--destination_user', action="store", dest="destination", help="The user ID of the person you want to send gifs to")
+parser.add_argument('-a','--add_gif', action="store", dest="more_gif", help="Gifs you want to add")
+parser.add_argument('-m','--message', action="store", dest="message", help="The message you want to send with the gif")
+
 
 args = parser.parse_args()
 
+more_gif = []
+if args.more_gif:
+    more_gif = args.more_gif.split(',')
+
+for i in more_gif:
+    if more_gif.split('.')[-1] != "gif":
+        print("The url address for a gif you add must end with a \'.gif\'")
+        os._exit(0)  #I know it's bad practice but I thought it would be more usefriendly rather than raise an exception which might be harder to go through
+        #raise ValueError("The url address for a gif must end with a \'.gif\'")
+
 while not args.address:
-    args.address = input("Please enter your adress:")
+    args.address = input("Please enter your email adress:")
 
 while not args.password:
-    args.password = input("Please enter your password:")
-    
+    sys.stdout.write("Please enter your password:")
+    args.password = getpass.getpass()
+
+
+
+#-----------------------------------------Printing friend list
+try:
+    client = Client(args.address, args.password)
+except ConnectionError:
+    print("Could not connect, please check your connection")
+    os._exit(0)
+except FBchatUserError:
+    print("Wrong password/email combinaison")
+    os._exit(0)
+
+choice = ""
+while choice != "y" and choice != "n":
+    choice = input("Do you want to print your friends list, with their ID? (Press \'y\' if you're not sure) [y/n]:")
+    #print(repr(choice))
+    #assert choice == "y"
+
+if choice.lower() == "y":
+    def getKey(user):
+        return user.name
+
+    users = client.fetchAllUsers()
+    print("Name\t\t\t\t| ID")
+    for user in sorted(users,key=getKey):
+        a=4
+        if len(user.name) > 6:
+            a = 3
+        if len(user.name) > 14:
+            a = 2
+        if len(user.name) > 22:
+            a = 1
+        if len(user.name) > 30:
+            a = 0
+        if user.uid != "0" and user.uid != 0:
+            print(user.name,a*"\t",user.uid)
+#-----------------------------------------------
+
 while not args.destination:
-    args.destination = input("Please enter the id of the person you want send gifs to:")
+    args.destination = input("Please enter the user ID of the person you want to send gifs to:")
 
-while not args.user_id:
-    args.user_id = input("Please enter your user id:")
+while not args.start_time:
+    args.start_time = input("Please specify at what hour should the program start being active (in a 24h format, like \'13:00\'):")
 
-time_start_hour = args.start_time.split(':')[0]
-time_start_min = args.start_time.split(':')[1]
-time_stop_hour = args.stop_time.split(':')[0]
-time_stop_min = args.stop_time.split(':')[1]
+while not args.stop_time:
+    args.stop_time = input("Please specify at what hour should the program stop being active:")
 
-#-----------------------------------------
+while not args.message:
+    args.message = input("If you want to attach a message with your gif, please write it:")
+
+
+time_start_hour = int(args.start_time.split(':')[0])
+time_start_min = int(args.start_time.split(':')[1])
+time_stop_hour = int(args.stop_time.split(':')[0])
+time_stop_min = int(args.stop_time.split(':')[1])
+
+#-------------------------------------create gif database
 
 gifs= ["https://media.tenor.co/images/564eac526a8af795c90ce5985904096e/tenor.gif",
        "https://media.tenor.co/images/5d5565fe47af258d83b4caa2a668ccfa/tenor.gif",
@@ -53,10 +113,10 @@ gifs= ["https://media.tenor.co/images/564eac526a8af795c90ce5985904096e/tenor.gif
        "https://media.tenor.co/images/77d90206206963c6aa5b05a2aa5c8c06/tenor.gif",
        "https://media.tenor.co/images/9c4a6d3cb294d01177a5b1e1544a5b9b/tenor.gif"]
 
-client = Client(args.address, args.password)
+for i in more_gif:
+    gifs.append(i)
 
-SO = "<your SO's ID>"
-me = "<yours>"
+#-----------------------------------------------
 
 def printf(text):
     sys.stdout.write(str(text)+"\n")
@@ -66,7 +126,7 @@ while True:
     printf("---------------------------New loop---------------------------")
     messages = client.fetchThreadMessages(thread_id=args.destination, limit=50)
     for message in messages:
-        if message.author == args.user_id:
+        if message.author == client.uid:
             del messages
             break
 
@@ -75,32 +135,30 @@ while True:
 
     printf("Last message: \"" + message.text + "\" @ " + str(time_last_message))
     printf("Current time: " + str(time_now.time()))
-    printf("Difference:" + str((time_now-time_last_message).seconds))
+    printf("Difference: " + str((time_now-time_last_message).seconds) + " seconds")
 
-    if time_now.time() > datetime.time(time_start_hour, time_start_min) and time_now.time() < datetime.time(time_stop_hour, time_stop_min): 
+    if time_now.time() > datetime.time(time_start_hour, time_start_min) and time_now.time() < datetime.time(time_stop_hour, time_stop_min):
 
         if (time_now-time_last_message).seconds > 3600:
             index=random.randrange(0,len(gifs))
-            printf("Sending message number " + str(index))
-            client.sendRemoteImage(gifs[index], message=Message(text="(petit calinou random <3 )"), thread_id=args.destination, thread_type=ThreadType.USER)
-            printf("Image sent")
+            printf("\033[92m{}" + str(index) +"\033[00m".format("Sending image \#"))
+            client.sendRemoteImage(gifs[index], message=Message(text=args.message), thread_id=args.destination, thread_type=ThreadType.USER)
+            printf("\033[92m{}\033[00m".format("Image sent"))
 
             delais = random.randrange(3600*2,3600*3)
-            printf("Sleeping during " + str(delais) + "seconds")
-            time.sleep(delais)
+
+
             #printf("Dors pendant " + str(delais) + "secondes")
 
         else:
             delais = random.randrange(0,3600*1)
-            printf("Sleeping during " + str(delais) + "seconds")
-            time.sleep(delais)
+            printf("\033[91m{}\033[00m".format("Last message sent less than an hour ago"))
+
 
     else:
         delais = random.randrange(3600*3,3600*5)
-        printf("Time doesn't fall in range, sleeping during " + str(delais) + "seconds")
-        time.sleep(delais)
+        printf("\033[91m{}\033[00m".format("Time doesn't fall in the active period"))
 
 
-
-#local_time = utc_time.astimezone()
-#print(local_time.strftime("%Y-%m-%d %H:%M:%S.%f%z (%Z)"))
+    printf("Sleeping during " + str(delais) + " seconds")
+    time.sleep(delais)
